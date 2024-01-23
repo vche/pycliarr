f3b135f884df704c85ab8699a9b756d69587dd82
Vivien Chene <viv.chene@gmail.com>
2024-01-23 14:55:50 +0000

    Update release to 1.0.25

--- release.sh
+++ release.sh
@@ -24,5 +24,5 @@ if [ $? -ne 0 ]; then
   exit 1 
 fi
 
-echo "--> Publish $1 release to pypi"
-python3 -m twine upload --repository pycliarr dist/pycliarr-$1*
+# echo "--> Publish $1 release to pypi"
+# python3 -m twine upload --repository pycliarr dist/pycliarr-$1*
