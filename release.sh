ce51e4a4a31b3b6b7a6cab937a7a0964ee07fa1c
Vivien Chene <viv.chene@gmail.com>
2024-01-23 14:51:05 +0000

    Update release to 1.0.24

--- release.sh
+++ release.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
-echo "Updating version to $1"
+echo "--> Updating version to $1"
 echo "__version__ = '$1'" > src/pycliarr/version.py
 
-echo "Building wheel version $1"
+echo "--> Building wheel version $1"
 pixi run release
 
-echo "Pushing commit and generating github release v$1"
-igit up -m "Update release to $1" && gh release create v$1 --generate-notes
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
 
-echo "Publish $1 release to pypi"
+echo "--> Pushing commit and generating github release v$1"
+igit up -m "Update release to $1"
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
+
+echo "--> Generating github release v$1"
+gh release create v$1 --generate-notes
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
+
+echo "--> Publish $1 release to pypi"
 python3 -m twine upload --repository pycliarr dist/pycliarr-$1*
